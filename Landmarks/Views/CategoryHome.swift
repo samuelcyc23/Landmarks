3ec2aca14a952ac6c2ded59f16b26349d8faac82
samuelcyc23 <144550391+samuelcyc23@users.noreply.github.com>
2023-10-09 23:10:49 +0800

    Composing Complex Interfaces - section 1

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Samuel CHENG on 9/10/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
